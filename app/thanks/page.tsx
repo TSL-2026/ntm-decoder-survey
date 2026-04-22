9a7cb724013429be71c1015d5897cfa9c75abc32
The Safety Layer <thsafetylayer@gmail.com>
2026-04-22 18:53:07 +0545

    Add Thanks component to display thank you message

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Thanks() {
+  return (
+    <main style={{ textAlign: "center", marginTop: 80 }}>
+      <h1>Thank you!</h1>
+      <p>Your response has been recorded.</p>
+    </main>
+  );
+}
